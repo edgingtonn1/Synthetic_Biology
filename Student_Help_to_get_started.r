599e5065004882e169ec0674d2d3030cc5dadfcc
n_edgington <edgingtonn1@southernct.edu>
2014-01-24 13:52:46 -0500

    readme
    readme file added

---
+++ Student_Help_to_get_started.r
@@ -0,0 +1,10 @@
+#Students: Set your working directory
+
+#If you haven't already, install 'shiny' in R studio:
+install.packages("shiny")
+
+#Load the shiny package:
+library(shiny)
+
+#Run a shiny app:
+shiny::runGitHub('SynthBio_oscillations', 'n_edgington')
