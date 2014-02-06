d3c7edfad41b44371b080ed2de4780a768f7d694
edgingtonn1 <edgingtonn1@southernct.edu>
2014-02-06 08:32:58 -0500

    Now using ggplot2 & stat_function

--- NE_ShinyApp_Logistic_Model/server.R
+++ NE_ShinyApp_Logistic_Model/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
@@ -7,9 +8,12 @@ shinyServer(function(input, output) {
 #K <- 10
     
    output$Plot <- renderPlot({
-    x <- seq(input$obs)
-    y <- (20*input$K)/(20+(input$K-20)*(exp(-(input$R)*x)))
-    plot(y ~ x, main="Logistic Model")
+    #x <- seq(input$obs)
+    logisticfxn <- function(xvar) {
+    	(20*input$K)/(20+(input$K-20)*(exp(-(input$R)*xvar)))
+    }
+    p <- ggplot(data.frame(x=seq(input$obs)), aes(x=x)) + stat_function(fun=logisticfxn)
+	print(p)  
   })
 })
 	
