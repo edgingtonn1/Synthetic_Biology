7a99c31914fb3c2100a96e4cd9dc6acc5fdf5c46
edgingtonn1 <edgingtonn1@southernct.edu>
2014-02-11 13:01:39 -0500

    Update Student_Help_to_get_started.r
    Added instructions to infall ggplot2 if necessary.

--- Student_Help_to_get_started.r
+++ Student_Help_to_get_started.r
@@ -3,8 +3,13 @@
 #If you haven't already, install 'shiny' in R studio:
 install.packages("shiny")
 
+#If you haven't already, install 'ggplot2' in R studio:
+install.packages("ggplot2")
+
 #Load the shiny package:
 library(shiny)
 
 #Run a shiny app:
 shiny::runGitHub("Synthetic_Biology","edgingtonn1", subdir="NE_ShinyApp_Logistic_Model/")
+
+# To run other models, just change the "subdir" directory
